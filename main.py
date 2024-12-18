6b209b185004492172d31b428a88c3056e37a060
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-12-18 12:06:36 +0100

    Изменено название функции с глагола Добавлен цикл Добавлена защита от ошибок в GET параметрах

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import requests
 
 
-def city_weather(city_url):
-    response = requests.get(city_url)
+def get_weather(city_url, params=None):
+    response = requests.get(city_url, params=params)
     response.raise_for_status()
     return response.text
 
 
 def main():
-    london_url = "https://wttr.in/london"
-    sheremetyevo_url = "https://wttr.in/svo"
-    cherepovets_url = "https://wttr.in/%D0%A7%D0%B5%D1%80%D0%B5%D0%BF%D0%BE%D0%B2%D1%86%D0%B0?m&M&n&q&T&lang=ru"
+    base_url = "https://wttr.in"
+    cities = [
+        ("london", None),
+        ("svo", None),
+        ("Череповец", {"m": "", "M": "", "n": "", "q": "", "T": "", "lang": "ru"})
+    ]
 
-    london_weather = city_weather(london_url)
-    sheremety_weather = city_weather(sheremetyevo_url)
-    cherepovets_weather = city_weather(cherepovets_url)
-
-    print(london_weather)
-    print(sheremety_weather)
-    print(cherepovets_weather)
+    for city, params in cities:
+        city_url = f"{base_url}/{city}"
+        city_weather = get_weather(city_url, params)
+        print(city_weather)
 
 
 if __name__ == "__main__":
